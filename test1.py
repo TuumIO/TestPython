6eccbae1f101db7497260a450f21017d38071acb
CarlosAndresB <carlosandresb@tuum.io>
2017-12-06 19:50:15 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -5,30 +5,28 @@ import getopt
 
 def usage():
        
-       print("Hello World")
        print("Usage: Adding Test")
        print("")
-       print(" --A (-f) First Number")
-       print(" --B (-s) Second Number")
-       print(" --help this screen")
+       print("-f First Number")
+       print("-s Second Number")
+       print("-h this screen")
 
 def main():
        'Main function'
        global Action, x, y, z
 
        try:
-              opts, args = getopt.getopt(sys.argv[1:], "f:s:h") # output=
+              opts, args = getopt.getopt(sys.argv[1:], "f:s:h")
 
        except getopt.GetoptError as err:
-              print("Error: "+str(err)+", Try --help for usage\n\n")
+              print("Error: "+str(err)+", Try -h for usage\n\n")
               usage()
               sys.exit(2)
 
        if len(opts) == 0:
-              print("Include number to add,  See --help for usage")
+              print("Include number to add,  See -h for usage")
               sys.exit()
-       
-        for o, a in opts:
+       for o, a in opts:
                 if o in ("-h", "--help"):
                         usage()
                         sys.exit()
@@ -37,19 +35,17 @@ def main():
                                 x=float(a)
                         except ValueError:
                                 print("--from argument is taking only numeric values")
-                                sys.exit(2);
+                                sys.exit(2)
 
                 if o in ("-s"):
                         try:
                                 y=float(a)
                         except ValueError:
                                 print ("--to argument is taking only numeric values")
-                                sys.exit(2);
-                
-                   
-        z = x + y
-        print(z)
-        print("Thats it")
+                                sys.exit(2)
+       z = x + y
+       print(z)
+       print("Thats it")
 
 if __name__ == "__main__":
        main()

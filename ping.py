bb078a919869fb79d26f6a757f318e2294b4e845
CarlosAndresB <carlosandresb@tuum.io>
2017-12-10 10:17:21 +0100

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,8 +1,43 @@
 import os
-hostname = "google.com"
-response = os.system("ping -c 1 " + hostname)
-if response == 0:
-    pingstatus = "Network Active"
-else:
-    pingstatus = "Network Error"
-print(pingstatus)
+import sys
+import getopt
+
+def usage():
+       
+       print("Uso: Ping")
+       print("")
+       print("-d Direccion")
+       print("-h info de ayuda")
+
+def main():
+       'Main function'
+       global Action, x, y, z
+
+       try:
+              opts, args = getopt.getopt(sys.argv[1:], "d:h")
+
+       except getopt.GetoptError as err:
+              print("Error: "+str(err)+", Intente -h para ayuda\n\n")
+              usage()
+              sys.exit(2)
+
+       for o, a in opts:
+                if o in ("-h"):
+                        usage()
+                        sys.exit()
+                if o in ("-d"):
+                        try:
+                            response = os.system("ping -c 1 " + hostname)
+                            if response == 0:
+                                pingstatus = "Ususario Activo"
+                            else:
+                                pingstatus = "Usuario no Activo"
+                            print(pingstatus)
+
+                        except:
+                            sys.exit(2)
+
+        print("Listo!")
+
+if __name__ == "__main__":
+main()

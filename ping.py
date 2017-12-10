a2cad204bdb1b9d1a10dcff9077128b9b15070c4
CarlosAndresB <carlosandresb@tuum.io>
2017-12-10 10:28:40 +0100

    Update ping.py

--- ping.py
+++ ping.py
@@ -9,31 +9,28 @@ def usage():
        print("-d Direccion")
        print("-h info de ayuda")
 
-def main():
 
-       try:
-              opts, args = getopt.getopt(sys.argv[1:], "d:h")
+try:
+       opts, args = getopt.getopt(sys.argv[1:], "d:h")
 
-       except getopt.GetoptError as err:
-              print("Error: "+str(err)+", Intente -h para ayuda\n\n")
+except getopt.GetoptError as err:
+       print("Error: "+str(err)+", Intente -h para ayuda\n\n")
+       usage()
+       sys.exit(2)
+
+for o, a in opts:
+       if o in ("-h"):
               usage()
-              sys.exit(2)
-
-       for o, a in opts:
-                if o in ("-h"):
-                        usage()
-                        sys.exit()
-                if o in ("-d"):
-                        try:
-                            response = os.system("ping -c 1 " + hostname)
-                            if response == 0:
-                                pingstatus = "Ususario Activo"
-                            else:
-                                pingstatus = "Usuario no Activo"
+              sys.exit()
+       if o in ("-d"):
+              try:
+                     response = os.system("ping -c 1 " + hostname)
+                     if response == 0:
+                            pingstatus = "Ususario Activo"
                             print(pingstatus)
-
-                        except:
-                            sys.exit(2)
-       print("Listo\n")
-if __name__ == "__main__":
-       main()
+                     else:
+                            pingstatus = "Usuario no Activo"
+                            print(pingstatus)
+              except:
+                     sys.exit(2)
+print("Listo\n")

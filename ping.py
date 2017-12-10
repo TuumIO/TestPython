bc77e3189aa34db22640d939d54cc328d714da94
CarlosAndresB <carlosandresb@tuum.io>
2017-12-10 12:17:12 +0100

    Update ping.py

--- ping.py
+++ ping.py
@@ -6,12 +6,13 @@ def usage():
        
        print("Uso: Ping")
        print("")
+       print("-f Fin")
        print("-d Direccion")
        print("-h info de ayuda")
 
 
 try:
-       opts, args = getopt.getopt(sys.argv[1:], "d:h")
+       opts, args = getopt.getopt(sys.argv[1:], "f:d:h")
 
 except getopt.GetoptError as err:
        print("Error: "+str(err)+", Intente -h para ayuda\n\n")
@@ -22,15 +23,19 @@ for o, a in opts:
        if o in ("-h"):
               usage()
               sys.exit()
+       if o in ("-f"):
+              x = a
        if o in ("-d"):
               try:
-                     response = os.system("ping -c 1 " + a)
-                     if response == 0:
-                            pingstatus = "Ususario Activo"
+                     for i < x:
+                            direc = a + "." + str(i)
+                            response = os.system("ping -c 1 " + direc)
+                            if response == 0:
+                                   pingstatus = "Ususario Activo"
+                            else:
+                                   pingstatus = "Usuario no Activo"
                             print(a + pingstatus)
-                     else:
-                            pingstatus = "Usuario no Activo"
-                            print(a + pingstatus)
+                            i++
               except:
                      sys.exit(2)
 print("Listo\n")

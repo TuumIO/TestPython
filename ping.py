04c6deae2325fa3ebc29d1cdbb4d8606e6f462d5
CarlosAndresB <carlosandresb@tuum.io>
2017-12-10 12:45:47 +0100

    Update ping.py

--- ping.py
+++ ping.py
@@ -2,6 +2,8 @@ import os
 import sys
 import getopt
 
+resultados = {} #Diccionario donde se guardan los resultados
+
 def usage():
        
        print("Uso: Ping")
@@ -32,12 +34,15 @@ for o, a in opts:
                             direc = a + "." + str(i)
                             response = os.system("ping -c 1 " + direc)
                             if response == 0:
+                                   resultados = {i:response}
                                    pingstatus = "Ususario Activo"
                             else:
+                                   resultados = {i:response}
                                    pingstatus = "Usuario no Activo"
                             print(direc + " " + pingstatus)
                             i=i+1
               except:
                      print("Algo pasa aca\n")
                      sys.exit(2)
+print(resultados)
 print("Listo\n")
